00fba73080889233dd0386d2f5fdb2078a8cb7b0
Shimshoni <tamirshimoni1@gmail.com>
2022-06-02 20:33:25 +0300

    GUI2

--- DemoApp/App.js
+++ DemoApp/App.js
@@ -1,19 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Linking, Button, Alert,
-   KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard} from 'react-native';
+   KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard, SafeAreaView, TouchableOpacity, Image} from 'react-native';
 import { TextInput } from 'react-native-web';
+import React,{Component} from 'react';
+
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>מספר תחנה:
-      
-      </Text>
-      
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+        <TouchableOpacity onPress={()=>console.log("bluetooth button tapped")}>
+          <Image 
+          source={{
+            width: 300,
+            height: 200,
+            uri:"https://img.favpng.com/3/10/22/computer-icons-symbol-bluetooth-png-favpng-9qXRbFELntZfwa2ABgY1fh696.jpg",
+          }}
+          />
+        </TouchableOpacity>
+        <StatusBar style="auto" />
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
